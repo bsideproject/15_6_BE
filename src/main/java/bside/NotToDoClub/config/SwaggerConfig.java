75d16f9b86ebfc3bd96494ddb2e8e15f8b73c080
sunga <eert2260@naver.com>
2023-05-22 23:31:41 +0900

    swagger 설정 추가

---
+++ src/main/java/bside/NotToDoClub/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package bside.NotToDoClub.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("bside")
+                .pathsToMatch("/**")
+                .build();
+    }
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("B-Side Project API")
+                        .description("비사이드 프로젝트 API 명세서입니다.")
+                        .version("v0.1"));
+    }
+}
