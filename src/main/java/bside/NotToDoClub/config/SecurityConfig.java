c07b014a2a809d2a1cf140342713bfe621dd1a31
sihun <34716571+dolgogae@users.noreply.github.com>
2023-06-22 23:47:55 +0900

    Feat: cors 설정 변경

--- src/main/java/bside/NotToDoClub/config/SecurityConfig.java
+++ src/main/java/bside/NotToDoClub/config/SecurityConfig.java
@@ -52,5 +52,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .exceptionHandling()
                 .authenticationEntryPoint(new MyAuthenticationEntryPoint())
                 .accessDeniedHandler(new MyAccessDeniedHandler());
+        http.cors().disable();
     }
 }
