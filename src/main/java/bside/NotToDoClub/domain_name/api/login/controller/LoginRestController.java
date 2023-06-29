d7f22d79fe169ecca2c6065309d2a6ac5c46c6c3
sihun <34716571+dolgogae@users.noreply.github.com>
2023-06-29 22:03:49 +0900

    feat: LoginRestController 로그 추가

--- src/main/java/bside/NotToDoClub/domain_name/api/login/controller/LoginRestController.java
+++ src/main/java/bside/NotToDoClub/domain_name/api/login/controller/LoginRestController.java
@@ -9,12 +9,13 @@ import bside.NotToDoClub.global.response.AuthResponse;
 import bside.NotToDoClub.global.response.ResponseCode;
 import bside.NotToDoClub.global.response.ResultResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@RestController
+@RestController @Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/login")
 public class LoginRestController {
@@ -24,18 +25,21 @@ public class LoginRestController {
 
     @GetMapping("/{socialLoginType}/page") //socialLoginType=naver, google, kakao...
     public void socialLoginRedirect(HttpServletResponse response, @PathVariable(name="socialLoginType") String SocialLoginPath) throws Exception {
+        log.info("call {} social login", SocialLoginPath);
         Constant.SocialLoginType socialLoginType= Constant.SocialLoginType.valueOf(SocialLoginPath.toUpperCase());
         response.sendRedirect(oAuthService.getRedirectUrl(socialLoginType));
     }
 
     @GetMapping("/auth/google-callback")
     public ResultResponse<AuthResponse> googleCallback(@RequestParam(name="code") String code) throws IOException{
+        log.info("google login code = {}", code);
         AuthResponse authResponse = loginService.googleLogin(code);
         return ResultResponse.of(ResponseCode.PROVIDE_APP_TOKEN, authResponse);
     }
 
     @PostMapping("/auth/kakao-callback")
     public ResultResponse<AuthResponse> kakaoCallback(@RequestBody AuthRequestDto authRequestDto) throws IOException{
+        log.info("kakao login code = {}", authRequestDto.getCode());
         AuthResponse authResponse = loginService.kakaoLogin(authRequestDto.getCode());
         return ResultResponse.of(ResponseCode.PROVIDE_APP_TOKEN, authResponse);
     }
