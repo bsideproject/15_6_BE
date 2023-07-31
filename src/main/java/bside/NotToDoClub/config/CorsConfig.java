60495bd61f53a47f2fc95d0b64da9af6ab773822
sihun <34716571+dolgogae@users.noreply.github.com>
2023-07-31 21:59:45 +0900

    fix: cors 설정 변경

--- src/main/java/bside/NotToDoClub/config/CorsConfig.java
+++ src/main/java/bside/NotToDoClub/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("http://localhost:8080"));
+        config.setAllowedOrigins(List.of("http://localhost:8080", "https://nottodoclub.store"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("*"));
