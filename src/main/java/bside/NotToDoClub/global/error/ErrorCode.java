b357207083e1dd7c286c566154d4d3b2d12d6d52
sunga <eert2260@naver.com>
2023-08-08 16:27:59 +0900

    fix: 절제기록 error code 다시 추가

--- src/main/java/bside/NotToDoClub/global/error/ErrorCode.java
+++ src/main/java/bside/NotToDoClub/global/error/ErrorCode.java
@@ -22,6 +22,8 @@ public enum ErrorCode {
     CHEER_UP_MESSAGE_NOT_FOUND(NOT_FOUND, "해당 낫투두 ID의 응원메시지를 찾을 수 없습니다."),
     ALREADY_USER_NOT_TO_DO_DELETE(NOT_FOUND, "삭제된 낫투두입니다."),
     INVALID_INQUIRY_ID(NOT_FOUND, "해당 문의사항을 찾을 수 없습니다."),
+    USER_MODERATION_RECORD_NOT_FOUND(NOT_FOUND, "사용자의 절제기록을 찾을 수 없습니다."),
+    MODERATION_RECORD_NOT_FOUND(NOT_FOUND, "해당 절제기록 ID의 절제기록을 찾을 수 없습니다."),
 
     /* 409 CONFLICT : Resource 의 현재 상태와 충돌. 보통 중복된 데이터 존재 */
     DUPLICATE_RESOURCE(CONFLICT, "데이터가 이미 존재합니다"),
